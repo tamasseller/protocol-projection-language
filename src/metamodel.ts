1df60c58d031094864d477b23b3bce6555eaa05f
Tamás Seller <seller.tamas@gmail.com>
2026-07-22 17:07:38 +0200

    prgrs?

--- src/metamodel.ts
+++ src/metamodel.ts
@@ -1,25 +1,33 @@
 /**
  * Semantic Metamodel AST Definitions
  */
+
 export type SemanticType = IntegerType | StructType | UnionType | ListType;
 
-export interface IntegerType {
+export type SemanticField = { name: string; type: SemanticType }
+
+export interface IntegerType 
+{
     kind: 'Integer';
     min: number;
     max: number;
 }
 
-export interface StructType {
-    kind: 'Struct';
-    fields: { name: string; type: SemanticType }[];
-}
-
-export interface UnionType {
-    kind: 'Union';
-    variants: { name: string; type: SemanticType }[];
-}
-
-export interface ListType {
+export interface ListType 
+{
     kind: 'List';
     elementType: SemanticType;
 }
+
+export interface StructType 
+{
+    kind: 'Struct';
+    fields: SemanticField[];
+}
+
+export interface UnionType 
+{
+    kind: 'Union';
+    variants: SemanticField[];
+}
+
